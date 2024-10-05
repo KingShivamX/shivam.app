12f1c90c93319696c398d29857ab139c3d757f88
KingShivamX <shivamhippalgave@gmail.com>
2024-10-05 21:37:19 +0530

    ahh

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react"
-
 const App = () => {
     return (
         <div className="flex items-center justify-center text-3xl sm:text-5xl font-bold h-screen w-full bg-black text-white">
             Hiii, <br /> well I made it till here.
             <br />
             <br /> ~ Shivam
+            <br />
+            Wanna Laugh ?{" "}
+            <a href="https://adityakotkar.pages.dev/">Click Here</a>
         </div>
     )
 }
