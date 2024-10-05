a6e1bf66e149a4199a84acf227387c9ee4c3f8c9
KingShivamX <shivamhippalgave@gmail.com>
2024-10-05 21:41:04 +0530

    yes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import React from "react"
 const App = () => {
     return (
-        <div className="flex items-center justify-center text-3xl sm:text-5xl font-bold h-screen w-full bg-black text-white">
+        <div className="flex flex-col items-center justify-center text-3xl sm:text-5xl font-bold h-screen w-full bg-black text-white">
             Hiii, <br /> well I made it till here.
             <br />
             <br /> ~ Shivam
             <br />
-            Wanna Laugh ?{" "}
-            <a href="https://adityakotkar.pages.dev/">Click Here</a>
+            <br />
+            <div>
+                Wanna Laugh ?
+                <a
+                    href="https://adityakotkar.pages.dev/"
+                    style={{ textDecoration: "underline" }}
+                >
+                    {" "}
+                    Click Here
+                </a>
+            </div>
         </div>
     )
 }
